8a5894ec88488ca495bd1ec6153fd7b11cb2403b
dennis <dennis.mwangi@student.moringaschool.com>
2023-06-18 00:12:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (activities="roller-skate") {
+    return `This Saturday, I want to ${activities}!`
+}
+function mondayWork (toDoList = "go to the office") {
+    return `This Monday, I will ${toDoList}.`
+}
+function  wrapAdjective (special="*") {
+    return function () {
+
+    }
+}
+
+
+
+
+
+
+
+    
